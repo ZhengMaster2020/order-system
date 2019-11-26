7732ed41e644cd7194ea017e7ea63917a77dd65e
hanxiaole <15038914412@163.com>
2019-11-26 17:35:38 +0800

    防伪码合作商

--- plugins/router.js
+++ plugins/router.js
@@ -8,5 +8,14 @@ export default [
       { path: 'security-code/management', title: '防伪码管理', name: 'security-code/management' },
       { path: 'security-code/search-code', title: '防伪码查询', name: 'security-code/search-code' }
     ]
+  },
+  {
+    path: '/anti-stock',
+    icon: 'ios-book',
+    name: '/anti-stock',
+    title: '防串货',
+    children: [
+      { path: 'anti-stock/cooperation', title: '合作商管理', name: 'anti-stock/cooperation' }
+    ]
   }
 ]
