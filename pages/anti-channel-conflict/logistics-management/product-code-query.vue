8794d185912e50bbd068d72d52542db80f66e42e
mayingyi <287512807@qq.com>
2019-11-27 10:41:05 +0800

    物流管理页面布局

---
+++ pages/anti-channel-conflict/logistics-management/product-code-query.vue
@@ -0,0 +1,193 @@
+<template>
+  <div class="code-management">
+    <Row class="search-form">
+      <Col :md="12">
+        <Input v-model="searchData.securityCode" placeholder="序列号" style="width:150px" clearable @on-enter="getList('search')"/>
+        <Input v-model="searchData.securityCode" placeholder="订单编号" style="width:150px" clearable @on-enter="getList('search')"/>
+        <Button type="primary" icon="ios-search" @click="getList('search')">查询</Button>
+        <Button type="primary" icon="md-download">导出数据</Button>
+      </Col>
+    </Row>
+    <Row class="margin-top-10">
+      <Table :columns="listData.columns" :data="listData.data" size="small" border highlight-row :loading="listData.loading"></Table>
+      <div style="margin: 10px;overflow: hidden">
+        <div class="pages-L">共 {{pageProps.totalCount}} 条</div>
+        <div style="float: right;">
+          <Page size="small" :total="pageProps.totalCount" :current="pageProps.page" show-sizer show-elevator @on-change="changePage" @on-page-size-change="changePageSize"></Page>
+        </div>
+      </div>
+    </Row>
+    <!-- 物流信息弹窗 -->
+    <Modal
+      v-model="logisticsData.modal"
+      title="物流信息"
+      width="900"
+      :styles="{top: '50%',marginTop: '-300px'}"
+    >
+      <div style="height: 350px;overflowY: scroll;padding: 0 10px">
+        <Table :columns="logisticsData.columns" :data="logisticsData.data"></Table>
+      </div>   
+      <div slot="footer">
+        <Button type="primary" @click="logisticsData.modal=false" >确定</Button>
+        <Button type="default" @click="logisticsData.modal=false">取消</Button>
+      </div>
+    </Modal>
+    <!-- 重新关联弹窗 -->
+    <Modal
+      v-model="linkData.modal"
+      title="重新关联"
+      :styles="{top: '50%',marginTop: '-230px'}"
+    >
+      <div style="padding: 0 50px 0 30px">
+        <Form :model="linkData.data" label-position="right" :label-width="100">
+          <FormItem label="箱码：" required>
+              <Input v-model="linkData.data.code"/>
+          </FormItem>
+          <FormItem label="已关联产品：">
+              <Input v-model="linkData.data.product" disabled/>
+          </FormItem>
+        </Form>
+      </div>   
+      <div slot="footer">
+        <Button type="primary" @click="relevance()" >关联</Button>
+        <Button type="default" @click="linkData.modal=false">取消</Button>
+      </div>
+    </Modal>
+  </div>  
+</template>
+<script>
+  export default {
+    data () {
+      return {
+        searchData: {
+          securityCode: '',
+          type: ''
+        },
+        linkData: {
+          modal: false,
+          data: {}
+        },
+        listData: {
+          columns: [
+            {key: 'index', type: 'index', title: '序号', maxWidth: 120, align: 'center'},
+            {key: 'securityCode', title: '序列号', align: 'center'},
+            {key: 'securityCode', title: '关联箱码', align: 'center'},
+            {key: 'securityCode', title: '订单编号', align: 'center'},
+            {key: 'securityCode', title: '产品信息', align: 'center'},
+            {
+              key: 'securityCodeLink',
+              title: '物流信息',
+              maxWidth: 200,
+              align: 'center',
+              render: (h, params) => {
+                return h('Tooltip', {
+                  props: {
+                    content: '查看',
+                    placement: 'top'
+                  }
+                }, [ h('Button', {
+                    props: {
+                      type: 'primary',
+                      icon: 'md-search'
+                    },
+                    on: {
+                      'click': () => {
+                        this.logisticsData.modal = true
+                      }
+                    }
+                  })
+                ])
+              }
+            },
+            {
+              key: 'securityCodeLink',
+              title: '操作',
+              maxWidth: 200,
+              align: 'center',
+              render: (h, params) => {
+                return h('Tooltip', {
+                  props: {
+                    content: '重新关联',
+                    placement: 'top'
+                  }
+                }, [ h('Button', {
+                    props: {
+                      icon: 'md-refresh'
+                    },
+                    on: {
+                      'click': () => {
+                        this.linkData.data.code = ''
+                        this.linkData.modal=true
+                      }
+                    }
+                  }, '关联')
+                ])
+              }
+            }
+          ],
+          data: [{serialCode: 'sd'}]
+        },
+        logisticsData: {
+          modal: false,
+          columns: [
+            {key: 'time', title: '日期', maxWidth: 100, align: 'center'},
+            {key: 'securityCode', title: '类型', maxWidth: 80,align: 'center'},
+            {key: 'securityCode', title: '内容', align: 'center'}
+          ],
+          data: [{time: '2019-10-20', securityCode: '发货'}]
+        },
+        pageProps: { // 列表分页属性
+          page: 1,
+          perPage: 10,
+          totalCount: 0
+        }
+      }
+    },
+    mounted() {
+      //this.getList()
+    },
+    methods:{
+      changePage (e) {
+        this.pageProps.page = e
+        //this.getList();
+      },
+      changePageSize (e) {
+        this.pageProps.perPage = e
+        //this.getList();
+      },
+      // 查询数据
+      getList () {
+        let params = this.searchData
+        params.page = this.pageProps.page
+        params.perPage = this.pageProps.perPage
+      },
+      // 关联
+      relevance () {
+        if(!this.linkData.data.code){
+          this.$Message.error('请填入箱码')
+        } else{
+          this.linkData.modal = false
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="less">
+.code-management {
+  .count {
+    padding: 20px 0px 20px 30px;
+    border: 2px solid #eee;
+    border-radius: 10px;
+  }
+  .ivu-form-item {
+    display: inline-block;
+    margin-bottom: 0px;
+  }
+  .search-form {
+    background-color: #eee;
+    margin-top: 10px;
+    padding: 10px 10px;
+  }
+}
+</style>
--- plugins/router.js
+++ plugins/router.js
@@ -8,5 +8,22 @@ export default [
       { path: 'security-code/management', title: '防伪码管理', name: 'security-code/management' },
       { path: 'security-code/search-code', title: '防伪码查询', name: 'security-code/search-code' }
     ]
+  },
+  {
+    path: '/anti-channel-conflict',
+    icon: 'ios-book',
+    name: '/anti-channel-conflict',
+    title: '防串货',
+    children: [
+      {
+        path: 'anti-channel-conflict/logistics-management',
+        title: '物流管理',
+        name: 'anti-channel-conflict/logistics-management',
+        children: [
+          { path: 'anti-channel-conflict/logistics-management/box-code-query', title: '箱码查询', name: 'anti-channel-conflict/logistics-management/box-code-query' },
+          { path: 'anti-channel-conflict/logistics-management/product-code-query', title: '产品码查询', name: 'anti-channel-conflict/logistics-management/product-code-query' }
+        ]
+      }
+    ]
   }
 ]
