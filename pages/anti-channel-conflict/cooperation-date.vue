f26e3cf595bbf71c53adbcba745506a4f47e32f0
hanxiaole <15038914412@163.com>
2019-12-04 15:55:10 +0800

    防窜货-列表修改

--- pages/anti-channel-conflict/cooperation-date.vue
+++ pages/anti-channel-conflict/cooperation-date.vue
@@ -1,76 +1,144 @@
 <template>
-  <div class="cooperation-date">
-    <Row>
-        <Col span="12">
-          <Select style="width:100px">
-            <Option v-for="i in 12" >{{i}}月</Option>
+  <div class="code-management">
+    <Row class="search-form" type="flex" justify="space-between">
+        <Col :md="12">
+          <Select v-model="month" style="width:120px">
+            <Option v-for="item in monthList" :value="item.value" :key="item.value">{{ item.label }}</Option>
           </Select>
         </Col>
-        <Col span="12">
-          {{company}}
+        <Col :md="12">
+          <div style="font-size:15px">广州分局设</div>
         </Col>
     </Row>
-    <br>
-    <Row>
-        <Col span="24">
-          <div id="businessChart">
-
-          </div>
-        </Col>
+    <Row class="margin-top-20">
+      <Col span="24">
+        <div id="myChart"></div>
+      </Col>
     </Row>
-  </div>
+  </div>  
 </template>
 <script>
 // 引入 ECharts 主模块
-var echarts = require('echarts/lib/echarts');
+var echarts = require('echarts/lib/echarts')
 // 引入柱状图
-require('echarts/lib/chart/bar');
+require('echarts/lib/chart/line')
 // 引入提示框和标题组件
-require('echarts/lib/component/tooltip');
-require('echarts/lib/component/title');
+require('echarts/lib/component/tooltip')
+// require('echarts/lib/component/title')
 export default {
-  data (){
+  data () {
     return {
-      company:'广州分局厂家',
-      option:{
-            title: {
-                text: 'ECharts 入门示例'
+      month: '2月',
+      monthList: [],
+      option: {
+        tooltip : {
+            trigger: 'axis',
+            axisPointer: {
+                label: {
+                    backgroundColor: '#6a7985',
+                    formatter: (params) => {
+                        return this.month + params.value + '日'
+                    }
+                }
+            }
+        },
+        legend: {
+            data: ['收货量', '箱码'],
+            align: 'left',
+            left: 10
+        },
+        grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+        },
+        xAxis : [
+            {
+                type : 'category',
+                boundaryGap : false,
+                name: '(日)',
+                data : []
+            }
+        ],
+        yAxis : [
+            {
+                type : 'value'
+            }
+        ],
+        series : [
+            {
+                name:'收货量',
+                type:'line',
+                smooth: true, // 曲线
+                areaStyle: {}, // 曲线下的阴影
+                data:[200, 132, 101, 134, 90, 230, 210,120, 132, 101, 134, 90, 230, 210,120, 132, 101, 134, 90, 230, 210,120, 132, 101, 134, 90, 230, 210,213,123,324]
             },
-            tooltip: {},
-            legend: {
-                data:['销量']
-            },
-            xAxis: {
-                data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-            },
-            yAxis: {},
-            series: [{
-                name: '销量',
-                type: 'bar',
-                data: [5, 20, 36, 10, 10, 20]
-            }]
-        }
+            {
+                name:'箱码',
+                type:'line',
+                data:[200, 132, 30, 134, 90, 230, 210,120, 132, 101, 134, 90, 230, 210,120, 132, 101, 134, 90, 230, 210,120, 132, 101, 134, 90, 230, 210,213,123,324]
+            }
+        ]
+      }
     }
   },
   mounted() {
-   this.echartsinit ()
+    this.getMonth();
+    this.addXDate();
+    this.echartsInit();
   },
   methods: {
-    echartsinit (){
-      var myChart = echarts.init(document.getElementById('businessChart'));
-      myChart.setOption(option);
+    getMonth () {
+      let i=0
+      while (i<12)
+      {
+        i++
+        this.monthList.push({
+          value: `${i}月`,
+          label: `${i}月`
+        })
+      }
+    },
+    addXDate() {
+      let i=0
+      while(i<32){
+        this.option.xAxis[0].data.push(i)
+        i++
+        }
+      },
+    echartsInit () {
+      // 找到容器
+      let myChart = echarts.init(document.getElementById('myChart'))
+      // 开始渲染
+      myChart.setOption(this.option)
+      // 图表自适应
+      window.onresize = myChart.resize
     }
   }
 }
 </script>
 <style lang="less">
-  .cooperation-date {
+.code-management {
+  .count {
+    padding: 20px 0px 20px 30px;
+    border: 2px solid #eee;
+    border-radius: 10px;
+  }
+  .ivu-form-item {
+    display: inline-block;
+    margin-bottom: 0px;
+  }
+  .search-form {
+    background-color: #eee;
+    margin-top: 10px;
+    padding: 10px 10px;
+  }
+}
+#myChart{
     width: 100%;
-    border:1px solid red;
-    #businessChart {
-      width: 80%;
-      height:500px;
-      border:1px solid red;
-    }
+    height: 480px;
+    margin-left: auto;
+    margin-right: auto;
   }
 </style>
--- pages/anti-channel-conflict/cooperation.vue
+++ pages/anti-channel-conflict/cooperation.vue
@@ -3,9 +3,9 @@
     <Row :gutter="8" class="search-form">
       <Col>
         <Input type="text" placeholder="合作商管理" style="width: 150px"></Input>
-          <Input type="text" placeholder="负责人" style="width: 150px"></Input>
-            <Button type="primary">查询</Button>
-            <Button type="primary" style="float:right">新增合作商</Button>
+        <Input type="text" placeholder="负责人" style="width: 150px"></Input>
+        <Button type="primary">查询</Button>
+        <Button type="primary" style="float:right" @click="addBusiness">新增合作商</Button>
       </Col>
     </Row>
     <Row style="margin-top:5px">
@@ -13,24 +13,42 @@
         <!-- 操作 -->
         <template slot-scope="{ row }" slot="action">
           <Tooltip placement="top" content="编辑" transfer>
-              <Button type="primary" size="small">
+              <Button type="primary" size="small" @click="partnerEidt">
                   <Icon type="md-create" />
               </Button>
           </Tooltip>
-          <Poptip confirm title="您确认删除这条内容吗？" transfer>
-              <Button size="small">
+          <Poptip confirm title="您确认删除这条内容吗？" transfer @on-ok="deleFordataList(row.id)">
+              <Button size="small" >
                   <Icon type="md-trash" />
               </Button>
           </Poptip>
         </template>
       </Table>
     </Row>
+    <!-- 新增合作商 model-->
+    <Modal
+        v-model="addmodal"
+        title="新增">
+         <Form ref="addruleBusiness" :label-width="80">
+          <FormItem label="合作商"  :rules="{ required: true, message: '请填写合作商名称', trigger: 'blur' }">
+            <Input  placeholder="合作商名称" style="width:300px"></Input>
+          </FormItem>
+          <FormItem label="负责人"  :rules="{ required: true, message: '请填写负责人', trigger: 'blur' }" >
+            <Input  placeholder="负责人"  style="width:300px"></Input>
+          </FormItem>
+          <FormItem label="所在地址"  :rules="{ required: true, message: '请填写所在地址', trigger: 'blur' }" >
+            <Input  placeholder="所在地址"  style="width:300px"></Input>
+          </FormItem>
+        </Form>
+    </Modal>
   </div>
 </template>
 <script>
 export default {
   data(){
     return {
+      //模态框增加合作商
+      addmodal: false,
       //合作商数据
       columns1: [
         {title: '合作商名称', key: 'name',align: 'center'},
@@ -61,13 +79,27 @@ export default {
     }
   },
   methods:{
-   
+    //新增合作商
+    addBusiness () {
+      this.addmodal = true;
+    },
+    //编辑合作商
+    partnerEidt () {
+      this.addmodal = true;
+    },
+    //删除合作商列表
+    deleFordataList (id) {
+      console.log(id);
+    }
+  },
+  mounted(){
+
   }
 }
 </script>
 <style lang="less">
     .cooper-title {
-        color: #000;
+        color: rgb(150, 126, 126);
         font-size: 18px;
     }
     .search-form {
