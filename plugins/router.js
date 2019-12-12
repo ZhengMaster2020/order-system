edea98798e864f1cf0bcea8a6e4e8571c57100d8
liyulin <919831912@qq.com>
2019-12-12 20:52:39 +0800

    update

--- plugins/router.js
+++ plugins/router.js
@@ -8,27 +8,20 @@ export default [
       { path: 'security-code/management', title: '防伪码管理', name: 'security-code/management' },
       { path: 'security-code/search-code', title: '防伪码查询', name: 'security-code/search-code' }
     ]
-<<<<<<< HEAD
-  }, 
-=======
   },
->>>>>>> origin/feature_account_management_mayingyi
   {
     path: '/anti-channel-conflict',
     icon: 'ios-book',
     name: '/anti-channel-conflict',
     title: '防串货',
     children: [
-<<<<<<< HEAD
       { path: 'anti-channel-conflict/cooperation', title: '合作商管理', name: 'anti-channel-conflict/cooperation' },
-      { path: 'anti-channel-conflict/factories-management', title: '加工厂管理', name: 'anti-channel-conflict/factories-management' }
-=======
+      { path: 'anti-channel-conflict/factories-management', title: '加工厂管理', name: 'anti-channel-conflict/factories-management' },
       {
         path: 'anti-channel-conflict/account-management/account-management',
         title: '账号管理',
         name: 'anti-channel-conflict/account-management/account-management'
       }
->>>>>>> origin/feature_account_management_mayingyi
     ]
   }
 ]
