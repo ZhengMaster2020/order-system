1ba39c195c6032de70645d0252b3d09de947c35a
shenyipeng <874537261@qq.com>
2020-05-20 15:48:14 +0800

    切换iview4

--- plugins/iview.js
+++ plugins/iview.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
-import iView from 'iview'
+import iView from 'view-design'
+import 'view-design/dist/styles/iview.css';
 import locale from 'iview/dist/locale/zh-CN' // Change locale, check node_modules/iview/dist/locale
 
 Vue.use(iView, {
