4d3cef6b3ce3d40a3dcc07534972217e648ec197
lqc <2507768471@qq.com>
2020-05-27 15:44:34 +0800

    Update aaa.sh

--- Jenkinsfile/aaa.sh
+++ Jenkinsfile/aaa.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 pm2=`type -p pm2`
 sitename=$1
+srcipts_dir=$2
+deploy_env=$3
 service_pid=`$pm2 pid ${sitename}`
+
+if [[ $deploy_env == "test" ]] || [[ $deploy_env == "rollback_test" ]];then
+    status=starttest
+else
+    status=start
+fi
 if [ $service_pid -ge 0 ];then
     $pm2 del $sitename
-    $pm2 start npm --name "${sitename}" -- run starttest
+    $pm2 start npm --name "${sitename}" -- run ${status}
     if [ $? -eq 0 ];then
         echo "服务启动成功"
     else
@@ -12,7 +20,7 @@ if [ $service_pid -ge 0 ];then
         exit 123
     fi
 else
-    $pm2 start npm --name "${sitename}" -- run starttest && echo "启动服务成功"
+    $pm2 start npm --name "${sitename}" -- run ${status} && echo "启动服务成功"
     if [ $? -eq 0 ];then
         echo "服务启动成功"
     else
@@ -21,9 +29,9 @@ else
     fi
 fi
 pwd
-rm -rf ./mkp_node_jenkinsfile
+rm -rf ${srcipts_dir}
 if [ $? -eq 0 ];then
-    echo "mkp_node_jenkinsfile脚本目录已删除"
+    echo "${srcipts_dir}脚本目录已删除"
 else
-    echo "删除失败，没有mkp_node_jenkinsfile这个文件夹，或者你没有权限对这个目录操作权限"
+    echo "删除失败，没有${srcipts_dir}这个文件夹，或者你没有权限对这个目录操作权限"
 fi
